6765fac915ee94fa25a2b522bff4d6f9057f3410
Pete Masse <pfmasse60@gmail.com>
2018-05-03 17:37:59 -0600

    Create keyvaluehash.cs

---
+++ keyvaluehash.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+					
+public class Program
+{
+	public static void Main()
+	{
+		var list = new List<KeyValuePair<int, int>>() { 
+                new KeyValuePair<int, int>(1, 0),
+				new KeyValuePair<int, int>(1, 0),
+				new KeyValuePair<int, int>(2, 0),
+				new KeyValuePair<int, int>(3, 0),
+				new KeyValuePair<int, int>(4, 0),
+				new KeyValuePair<int, int>(5, 0),
+				new KeyValuePair<int, int>(6, 0),
+				new KeyValuePair<int, int>(7, 0),
+				new KeyValuePair<int, int>(8, 0),
+            };
+            
+	}
+}
